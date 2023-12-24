4a7d16bd11b1326a53d54e7df26da2f0f22142af
Sarthak Sharma <sarthak2232@gmail.com>
2023-12-24 10:36:00 +0530

    separated components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,19 @@
 import React, { useState } from "react";
 import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
 
 function App() {
-  const [itemTitle, setItem] = useState("");
   const [itemList, setList] = useState([]);
 
-  const handleChange = (event) => {
-    const newValue = event.target.value;
-    setItem(newValue);
-  };
 
-  function addItem() {
+  function addItem(itemTitle) {
     setList((preItems) => [...preItems, itemTitle]);
-    setItem("");
   }
   function deleteItem(id) {
-    setList((preItems)=>{
-        return preItems.filter(
-            (item,index)=>{
-                return index!=id;
-            }
-        );
+    setList((preItems) => {
+      return preItems.filter((item, index) => {
+        return index != id;
+      });
     });
   }
 
@@ -29,12 +22,9 @@ function App() {
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input onChange={handleChange} type="text" value={itemTitle} />
-        <button onClick={addItem}>
-          <span>Add</span>
-        </button>
-      </div>
+      <InputArea
+        addInput={addItem}
+      />
       <div>
         <ul>
           {itemList.map((todoItem, itemIndex) => (
--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,22 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 
 function ToDoItem(props) {
-  const [isDone, setIsDone] = useState(false);
+  // const [isDone, setIsDone] = useState(false);
 
-  function handleClick() {
-    setIsDone((preValue) => {
-      return !preValue;
-    });
-  }
+  // function handleClick() {
+  //   setIsDone((preValue) => {
+  //     return !preValue;
+  //   });
+  // }
   return (
     <li
       onClick={
         () => props.isChecked(props.id)
       }
-      style={{ textDecoration: isDone ? "line-through" : "none" }}
+      // style={{ textDecoration: isDone ? "line-through" : "none" }}
     >
       {props.name}
     </li>
   );
 }
 export default ToDoItem;
+
+//commented code is for line-through decoration when user click on an item
